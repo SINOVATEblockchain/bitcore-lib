299a06a0fbbcb96df95b0da8da440f05396d04b1
Charly Dubois <charly.dubois.fr@gmail.com>
2020-01-07 17:00:41 +0100

    add module in bitcore-lib
    Signed-off-by: Charly Dubois <charly.dubois.fr@gmail.com>

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ global._bitcore = bitcore.version;
 bitcore.crypto = {};
 bitcore.crypto.BN = require('./lib/crypto/bn');
 bitcore.crypto.ECDSA = require('./lib/crypto/ecdsa');
+bitcore.crypto.ECDH = require('./lib/crypto/ecdh');
 bitcore.crypto.Hash = require('./lib/crypto/hash');
 bitcore.crypto.Random = require('./lib/crypto/random');
 bitcore.crypto.Point = require('./lib/crypto/point');
